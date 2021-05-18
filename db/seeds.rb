c8093c56c57426e92a8dd6de7fc5bcff624aab0a
Mickael FOSSO <fosso.mickael@gmail.com>
2021-05-18 15:49:49 +0200

    application logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Lorem.sentence
+  )
+  article.save!
+end
